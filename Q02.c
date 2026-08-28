3758eb519b986f86dc759b4a63de4b774f24c795
anshtiwari20aug-prog <anshtiwari20aug@gmail.com>
2026-08-28 23:43:38 +0530

    Day 01: Complete Q1 and Q2

---
+++ Q02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    double a, b;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);
+    printf("Sum = %.2lf\n", a + b);
+    printf("Difference = %.2lf\n", a - b);
+    printf("Product = %.2lf\n", a * b);
+    if (b != 0) printf("Quotient = %.2lf\n", a / b);
+    else printf("Division by zero is not possible.\n");
+    return 0;
+}
